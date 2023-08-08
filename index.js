bc54d96d025d6509104ca1380a4029629e54f730
Andrew <andrewstaveley14@gmail.com>
2023-08-08 00:10:12 -0400

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution in this file!
+const employee = {
+    name: "Andrew Staveley",
+    streetAddress: "123 Main Street",
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const employeeUpdated = {...employee};
+    employeeUpdated[key] = value;
+    return employeeUpdated;
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+};
+
+function deleteFromEmployeeByKey(employee, key) {
+    const employeeDeleted = {...employee};
+    delete employeeDeleted[key];
+    return employeeDeleted;
+};
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+};
